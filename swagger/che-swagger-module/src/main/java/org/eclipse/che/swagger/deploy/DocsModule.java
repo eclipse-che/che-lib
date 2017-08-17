32a4932830b8884e9fe70e0c84db57414c007a7a
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-08-17 15:29:15 +0300

    Used Google Java Codestyle

--- swagger/che-swagger-module/src/main/java/org/eclipse/che/swagger/deploy/DocsModule.java
+++ swagger/che-swagger-module/src/main/java/org/eclipse/che/swagger/deploy/DocsModule.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2012-2017 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,31 +7,26 @@
  *
  * Contributors:
  *   Red Hat, Inc. - initial API and implementation
- *******************************************************************************/
+ */
 package org.eclipse.che.swagger.deploy;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import com.google.inject.name.Names;
-
 import org.apache.commons.lang3.StringUtils;
 
-/**
- * @author Sergii Kabashniuk
- */
+/** @author Sergii Kabashniuk */
 public class DocsModule extends AbstractModule {
 
-    @Override
-    protected void configure() {
+  @Override
+  protected void configure() {
 
-        bind(org.eclipse.che.swagger.rest.SwaggerSpecificationService.class);
-        bind(org.eclipse.che.swagger.rest.SwaggerSerializers.class).asEagerSingleton();
-        //trim is a fake to make this module dependent to commons lang3 and have correct version. this is need for dependency convergence.
-        final Multibinder<Class> ignoredCodenvyJsonClasses =
-                Multibinder.newSetBinder(binder(), Class.class, Names.named(StringUtils.trim("che.json.ignored_classes")));
-        ignoredCodenvyJsonClasses.addBinding().toInstance(io.swagger.models.Swagger.class);
-
-    }
-
+    bind(org.eclipse.che.swagger.rest.SwaggerSpecificationService.class);
+    bind(org.eclipse.che.swagger.rest.SwaggerSerializers.class).asEagerSingleton();
+    //trim is a fake to make this module dependent to commons lang3 and have correct version. this is need for dependency convergence.
+    final Multibinder<Class> ignoredCodenvyJsonClasses =
+        Multibinder.newSetBinder(
+            binder(), Class.class, Names.named(StringUtils.trim("che.json.ignored_classes")));
+    ignoredCodenvyJsonClasses.addBinding().toInstance(io.swagger.models.Swagger.class);
+  }
 }
-
