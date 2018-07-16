9da56a17f5c82694118151e4b57f3f4367d0a3ad
Mykhailo Kuznietsov <mkuznets@redhat.com>
2018-07-16 18:05:49 +0300

    Use artifact implementation version as swagger api version

--- swagger/che-swagger-module/src/main/java/org/eclipse/che/swagger/deploy/BasicSwaggerConfigurationModule.java
+++ swagger/che-swagger-module/src/main/java/org/eclipse/che/swagger/deploy/BasicSwaggerConfigurationModule.java
@@ -10,8 +10,15 @@
  */
 package org.eclipse.che.swagger.deploy;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.servlet.ServletModule;
+import java.io.File;
+import java.net.URL;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import org.slf4j.LoggerFactory;
 
 /**
  * Module provide basic swagger configuration
@@ -19,6 +26,9 @@ import com.google.inject.servlet.ServletModule;
  * @author Sergii Kabashnyuk
  */
 public class BasicSwaggerConfigurationModule extends ServletModule {
+  private static final org.slf4j.Logger LOG =
+      LoggerFactory.getLogger(BasicSwaggerConfigurationModule.class);
+
   @Override
   protected void configureServlets() {
     bind(io.swagger.jaxrs.config.DefaultJaxrsConfig.class).asEagerSingleton();
@@ -26,8 +36,22 @@ public class BasicSwaggerConfigurationModule extends ServletModule {
         .with(
             io.swagger.jaxrs.config.DefaultJaxrsConfig.class,
             ImmutableMap.of(
-                "api.version", "1.0",
+                "api.version", getCheVersion(),
                 "swagger.api.title", "Eclipse Che",
                 "swagger.api.basepath", "/api"));
+  }
+
+  private String getCheVersion() {
+    try {
+      URL url =
+          BasicSwaggerConfigurationModule.class.getProtectionDomain().getCodeSource().getLocation();
+      try (JarFile jar = new JarFile(new File(url.toURI()))) {
+        final Manifest manifest = requireNonNull(jar.getManifest(), "Manifest must not be null");
+        return manifest.getMainAttributes().getValue("Implementation-Version");
+      }
+    } catch (Exception e) {
+      LOG.warn("Unable to retrieve implementation version from manifest file", e);
+      return "unknown";
+    }
   }
 }
