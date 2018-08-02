a619c99058d493095a9700128b319c03b90ee607
Sergii Kabashniuk <skabashnyuk@users.noreply.github.com>
2018-08-02 15:14:53 +0300

    Added dummy class to force maven to generate sources and javadoc artifact (#100)

---
+++ che-tomcat8-slf4j-logback/src/main/java/App.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which is available at http://www.eclipse.org/legal/epl-2.0.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+/** This is a dummy class to force maven to generate sources and javadoc artifacts */
+public class App {
+
+  public static void main(String[] args) {}
+}
